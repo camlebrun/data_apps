562ad6ee94a2374da0b979cb5d27c26530cb8c62
FONG026 <93886335+FONG026@users.noreply.github.com>
2024-03-12 23:39:56 +0100

    Update best_pyt_meth.py
    Functions Added

--- apps/script/best_pyt_meth.py
+++ apps/script/best_pyt_meth.py
@@ -1,1 +1,26 @@
+class BestPayMeth:
+    @staticmethod
+    def clean(df):
+        import pandas as pd
+      
+      # Read the payment dataframe
+        payment = pd.read_csv('data/olist_order_payments_dataset.csv')
 
+
+        return payment
+    @staticmethod
+    def kpi(df):
+
+        
+        # Group by to have the list of 
+        
+        paym_type=payment_type_rd.groupby('payment_type')['payment_value'].sum().reset_index()
+
+
+        # Displaying the total sales per state
+        print(paym_type)
+
+
+
+
+        return {'Sales per states': sales_per_state}
