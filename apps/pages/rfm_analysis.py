8df69190848b27f89a259db2cc94f94d9526f5fd
Camille Lebrun <c_lebrun3@hetic.eu>
2024-03-13 23:09:52 +0100

    feat: :sparkles: rfm segmentation

--- apps/pages/rfm_analysis.py
+++ apps/pages/rfm_analysis.py
@@ -3,13 +3,14 @@ import pandas as pd
 import plotly.express as px
 from script.gold.rfm import RFMAnalysis
 
+st.set_page_config(page_title='RFM Analysis', layout='wide')
 # Load the data
 df = pd.read_csv('data/cleaned_payments.csv')
 
 # Introduction
 st.title('RFM Analysis Dashboard')
 st.write('Welcome to the RFM Analysis Dashboard! This dashboard provides insights into customer behavior based on Recency, Frequency, and Monetary (RFM) analysis.')
-
+st.info('RFM analysis is a marketing technique used to determine quantitatively which customers are the best ones by examining how recently a customer has purchased (Recency), how often they purchase (Frequency), and how much the customer spends (Monetary).' )
 # Sidebar
 st.sidebar.title('Options')
 export_button = st.sidebar.button('Export RFM data to CSV')
@@ -17,52 +18,117 @@ export_button = st.sidebar.button('Export RFM data to CSV')
 # Use the RFMAnalysis class to perform RFM analysis
 rfm_analysis = RFMAnalysis(df)  
 rfm_result = rfm_analysis.calculate_rfm()
+col1, col2, col3 = st.columns(3)
 
 # Recency distribution (R)
-st.header('Recency Distribution (R)')
-st.write('Recency refers to the number of days since the customer\'s last purchase.')
 fig_recency = px.histogram(rfm_result, x='Recency', nbins=20, title='Recency Distribution (R)')
-st.plotly_chart(fig_recency)
 
 # Frequency distribution (F)
-st.header('Frequency Distribution (F)')
-st.write('Frequency represents the number of purchases made by each customer.')
 fig_frequency = px.histogram(rfm_result, x='Frequency', nbins=20, title='Frequency Distribution (F)')
-st.plotly_chart(fig_frequency)
 
 # Monetary value distribution (M)
 st.header('Monetary Value Distribution (M)')
 st.write('Monetary value reflects the total amount spent by each customer.')
 fig_monetary = px.histogram(rfm_result, x='Monetary', nbins=20, title='Monetary Value Distribution (M)')
-st.plotly_chart(fig_monetary, use_container_width=True)
 
 # Summary statistics for RFM
-st.header('Summary Statistics for RFM')
-st.write('Here are the summary statistics for Recency, Frequency, and Monetary.')
 st.dataframe(rfm_result.describe(), use_container_width=True)
+
+# boxplot for each RFM
+st.header('RFM Boxplot')
+st.write('The boxplots below show the distribution of Recency, Frequency, and Monetary values.')
+
+# Creating individual boxplots for Recency, Frequency, and Monetary
+fig_recency_box = px.box(rfm_result, y='Recency', title='Recency Boxplot')
+fig_frequency_box = px.box(rfm_result, y='Frequency', title='Frequency Boxplot')
+fig_monetary_box = px.box(rfm_result, y='Monetary', title='Monetary Boxplot')
+
+# Displaying the boxplots on 3 columns
+col1_box, col2_box, col3_box = st.columns(3)
+with col1_box:
+    st.plotly_chart(fig_recency_box, use_container_width=True)
+with col2_box:
+    st.plotly_chart(fig_frequency_box, use_container_width=True)
+with col3_box:
+    st.plotly_chart(fig_monetary_box, use_container_width=True)
 
 # Top 10 customers for each RFM
 st.header('Top 10 Customers for each RFM')
 st.write('Here are the top 10 customers based on Recency, Frequency, and Monetary.')
+
+
 top_recency = rfm_result.sort_values(by='Recency').head(10)
 top_frequency = rfm_result.sort_values(by='Frequency', ascending=False).head(10)
 top_monetary = rfm_result.sort_values(by='Monetary', ascending=False).head(10)
-st.subheader('Top 10 by Recency')
-st.dataframe(top_recency,use_container_width=True)
-st.subheader('Top 10 by Frequency')
-st.dataframe(top_frequency,use_container_width=True)
-st.subheader('Top 10 by Monetary Value')
-st.dataframe(top_monetary,use_container_width=True)
 
-# RFM Segmentation visualization
+
+with col1:
+    st.write('### Recency Distribution (R)')
+    st.write('Recency refers to the number of days since the customer\'s last purchase.')
+    st.plotly_chart(fig_recency, use_container_width=True)
+  
+with col2:
+    st.write('### Frequency Distribution (F)')
+    st.write('Frequency represents the number of purchases made by each customer.')
+    st.plotly_chart(fig_frequency, use_container_width=True)
+
+with col3:
+    st.write('### Monetary Value Distribution (M)')    
+    st.write('Monetary value reflects the total amount spent by each customer.')
+    st.plotly_chart(fig_monetary, use_container_width=True)
+
+colr, colf, colm = st.columns(3)
+with colr:
+    st.subheader('Top 10 by Recency')
+    st.dataframe(top_recency, use_container_width=True)
+with colf:
+    st.subheader('Top 10 by Frequency')
+    st.dataframe(top_frequency, use_container_width=True)
+with colm:
+    st.subheader('Top 10 by Monetary Value')
+    st.dataframe(top_monetary, use_container_width=True)
+
 st.header('RFM Segmentation')
-st.write('RFM Segmentation divides customers into four segments based on their RFM scores.')
-rfm_result['RFM_Score'] = rfm_result[['Recency', 'Frequency', 'Monetary']].sum(axis=1)
-rfm_result['RFM_Segment'] = pd.qcut(rfm_result['RFM_Score'], q=4, labels=['Low', 'Medium', 'High', 'Very High'])
-rfm_counts = rfm_result['RFM_Segment'].value_counts().reset_index()
-rfm_counts.columns = ['RFM_Segment', 'Number of customers']
-fig_segmentation = px.bar(rfm_counts, x='RFM_Segment', y='Number of customers', title='RFM Segmentation')
-st.plotly_chart(fig_segmentation)
+rfm_data_with_labels = rfm_analysis.calculate_labels(rfm_result)
+
+# Further processing and visualization
+rfm_data_with_labels['RFM_Segment'] = rfm_data_with_labels['recency_label'].astype(str) + rfm_data_with_labels['frequency_label'].astype(str) + rfm_data_with_labels['monetary_label'].astype(str)
+rfm_data_with_labels['RFM_Score'] = rfm_data_with_labels[['recency_label', 'frequency_label', 'monetary_label']].sum(axis=1)
+rfm_data_with_labels['RFM_Score'] = rfm_data_with_labels['RFM_Score'].astype(int)
+rfm_data_with_labels['RFM_Segment'] = rfm_data_with_labels['RFM_Segment'].astype(int)
+
+# Displaying the results
+st.dataframe(rfm_data_with_labels, use_container_width=True)
+with st.expander("RFM Label Calculation", expanded=True):
+    st.markdown("""
+    This function computes RFM labels for each customer based on their Recency, Frequency, and Monetary (RFM) values. 
+    Here's how it works:
+
+    - **Recency Labels:** Customers are categorized into four groups based on their recency of purchase. 
+      The recency labels are determined by dividing the range of recency values into quartiles:
+      - *Label 1:* Least recent customers
+      - *Label 2:* Moderately recent customers
+      - *Label 3:* Fairly recent customers
+      - *Label 4:* Most recent customers
+
+    - **Monetary Labels:** Customers are categorized into four groups based on their monetary value of purchases. 
+      The monetary labels are determined by dividing the range of monetary values into quartiles:
+      - *Label 4:* High-spending customers
+      - *Label 3:* Moderate-spending customers
+      - *Label 2:* Low-spending customers
+      - *Label 1:* Minimal-spending customers
+
+    - **Frequency Labels:** Customers are categorized into four groups based on the frequency of their purchases:
+      - *Label 4:* Most frequent customers
+      - *Label 3:* Moderately frequent customers
+      - *Label 2:* Infrequent customers
+      - *Label 1:* Least frequent customers
+
+    - **RFM Rank and RFM Rank RM:** These columns combine the Recency, Frequency, and Monetary labels to create a comprehensive RFM ranking for each customer. RFM Rank RM excludes Frequency from the ranking.
+
+    The resulting DataFrame includes these RFM labels and rankings for further analysis and segmentation of customers.
+    """)
+
 
 # Export RFM data
 if export_button:
--- apps/script/gold/rfm.py
+++ apps/script/gold/rfm.py
@@ -1,4 +1,5 @@
 import pandas as pd
+
 class RFMAnalysis:
     def __init__(self, df_data):
         self.df_data = df_data
@@ -6,7 +7,7 @@ class RFMAnalysis:
 
     def calculate_recency(self):
         recency = self.df_data.groupby('customer_unique_id', as_index=False)['order_purchase_timestamp'].max()
-        recency.rename(columns={'order_purchase_timestamp': 'LastPurchaseDate'}, inplace=True)  # Renommer correctement la colonne
+        recency.rename(columns={'order_purchase_timestamp': 'LastPurchaseDate'}, inplace=True)
         recent_date = self.df_data['order_purchase_timestamp'].dt.date.max()
         recency['Recency'] = recency['LastPurchaseDate'].dt.date.apply(lambda x: (recent_date - x).days)
         return recency[['customer_unique_id', 'Recency']]
@@ -27,5 +28,59 @@ class RFMAnalysis:
         monetary = self.calculate_monetary()
 
         rfm = recency.merge(frequency, on='customer_unique_id')
-        rfm = rfm.merge(monetary, on='customer_unique_id')  # Ne pas supprimer la colonne 'LastPurchaseDate' ici
+        rfm = rfm.merge(monetary, on='customer_unique_id')
+        return rfm
+
+    def calculate_labels(self, rfm):
+        # Labels for Recency
+        ll_r = rfm['Recency'].quantile(0.25)
+        mid_r = rfm['Recency'].quantile(0.50)
+        ul_r = rfm['Recency'].quantile(0.75)
+
+        def recency_label(recent):
+            if recent <= ll_r:
+                return 1
+            elif (recent > ll_r) and (recent <= mid_r):
+                return 2
+            elif (recent > mid_r) and (recent <= ul_r):
+                return 3
+            elif recent > ul_r:
+                return 4
+
+        rfm['recency_label'] = rfm['Recency'].apply(recency_label)
+
+        # Labels for Monetary
+        ll_m = rfm['Monetary'].quantile(0.25)
+        mid_m = rfm['Monetary'].quantile(0.50)
+        ul_m = rfm['Monetary'].quantile(0.75)
+
+        def monetary_label(money):
+            if money <= ll_m:
+                return 4
+            elif (money > ll_m) and (money <= mid_m):
+                return 3
+            elif (money > mid_m) and (money <= ul_m):
+                return 2
+            elif money > ul_m:
+                return 1
+
+        rfm['monetary_label'] = rfm['Monetary'].apply(monetary_label)
+
+        # Labels for Frequency
+        def frequency_label(frequent):
+            if frequent == 1:
+                return 4
+            elif frequent == 2:
+                return 3
+            elif frequent == 3:
+                return 2
+            elif frequent > 3:
+                return 1
+
+        rfm['frequency_label'] = rfm['Frequency'].apply(frequency_label)
+
+        # Create RFM rank and rank_rm columns
+        rfm['Rank'] = list(zip(rfm['recency_label'], rfm['monetary_label'], rfm['frequency_label']))
+        rfm['rank_rm'] = list(zip(rfm['recency_label'], rfm['monetary_label']))
+
         return rfm
