41bef1ba621610cd359f1616a35f836bc99b4165
FONG026 <93886335+FONG026@users.noreply.github.com>
2024-03-14 12:01:21 +0100

    Update payments_analysis.py
    Pie chart for order status

--- apps/pages/payments_analysis.py
+++ apps/pages/payments_analysis.py
@@ -42,8 +42,14 @@ st.plotly_chart(fig, use_container_width=True)
 st.header('Orders status')
 result = calculator.orders_status()
 orders_status = result['Orders status']
-fig = px.bar(x=orders_status.index, y=orders_status, title='Orders Status', labels={'y': 'Percentage'})
+fig = px.pie(values=orders_status, names=orders_status.index, title='Orders Status')
 st.plotly_chart(fig, use_container_width=True)
+
+#st.header('Orders status')
+#result = calculator.orders_status()
+#orders_status = result['Orders status']
+#fig = px.bar(x=orders_status.index, y=orders_status, title='Orders Status', labels={'y': 'Percentage'})
+#st.plotly_chart(fig, use_container_width=True)
 
 # Displaying Average Order Value, Max Order Value, Min Order Value, Total Orders, and Total Revenue
 st.header('Order Statistics')
