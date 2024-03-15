e9f3cb2110f8d8444ec01c04e67aa037619b493f
FONG026 <93886335+FONG026@users.noreply.github.com>
2024-03-15 10:05:24 +0100

    Update 2_Orders.py (#8)
    Added the orders density per region 
    
    Disclaimer: Running the app might be slower now

--- apps/pages/2_Orders.py
+++ apps/pages/2_Orders.py
@@ -10,6 +10,12 @@ st.markdown(
     "<h1 style='text-align: center;'>Orders</h1>",
     unsafe_allow_html=True)
 
+# Creation d'un plot pour la geolocalisation
+geolocation = pd.read_csv('data/olist_geolocation_dataset.csv')
+data = geolocation
+st.header('Orders per Region')
+st.map(data, latitude='geolocation_lat', longitude='geolocation_lng', use_container_width=True)
+
 calculator = KpiCalculator('data/cleaned_payments.csv')
 
 # Custom page names in the sidebar
